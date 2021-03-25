20cd9f1a480ccdee0468d755464e550494badb93
Casey Cameron <mcasey.cameron@gmail.com>
2021-03-25 15:19:12 -0700

    working rps game and tally

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-import { getRandomThrow } from './utils.js';
+import { getRandomThrow, didUserWin } from './utils.js';
 
 const userChoiceEl = document.getElementById('user-choice-div');
 let computerChoiceEl = document.getElementById('computer-choice-div');
 const userImageEl = document.getElementById('user-choice-image');
 const computerImageEl = document.getElementById('computer-choice-image');
+const resultsEL = document.getElementById('results-div');
 const winsEl = document.getElementById('wins-div');
 const lossesEl = document.getElementById('losses-div');
 const drawsEl = document.getElementById('draws-div');
 const throwButton = document.getElementById('throw-button-div');
 const resetButton = document.getElementById('reset-button-div');
 
+let gameTally = {
+    wins: 0,
+    losses: 0,
+    draws: 0,
+};
+
 throwButton.addEventListener('click', () => {
+    //reset resultsEl.textContent to default value
+
     // make computer choose rock, paper, or scissors
-     let computerChoice = getRandomThrow();
+    let computerChoice = getRandomThrow();
     // next, grab the user choice from the radio buttons
     const selectedChoice = document.querySelector('input[type=radio]:checked');
     let userChoice = selectedChoice.value;
 
-    // calculate if user won with a function
+    // calculate if user won with a function, 
+    let result = didUserWin(userChoice, computerChoice);
 
-    //update DOM
-    // computerChoiceEl.textContent
-
-
+    //update DOM with result using an if statement
+    if (result === 'win'){
+        resultsEL.textContent = 'You win!';
+        winsEl.textContent = 'Wins: ' + gameTally.wins++;
+    }
+    if (result === 'draw'){
+        resultsEL.textContent = "It's a draw.";
+        drawsEl.textContent = 'Draws: ' + gameTally.draws++; 
+    }
+    if (result === 'loss'){
+        resultsEL.textContent = 'You lose.';
+        lossesEl.textContent = 'Losses: ' + gameTally.losses++;
+    }
 });
